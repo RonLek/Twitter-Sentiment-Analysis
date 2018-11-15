725bb0a294832a33c22cce2c8a0b2feeb4d598b0
Rohan Lekhwani <rohanlekhwani@ymail.com>
2018-11-15 20:47:23 +0530

    Removed Twitter API Access and Consumer Tokens

--- twittersentimentanalysis.py
+++ twittersentimentanalysis.py
@@ -1,11 +1,16 @@
+#Rohan Lekhwani
+#Completed on - 10/11/18
+
 import tweepy
 from textblob import TextBlob
 
-consumer_key = 'Z3BGBjKgkL3ct6bk03TJ76ZF0'
-consumer_secret = 'EvDBuHPFrvVEv51G4AoxCEt6YE6o8fQ92OgINKvDGJiu0X3oDI'
+#Add Consumer Key and Consumer Secret from https://developer.twitter.com
+consumer_key = ''
+consumer_secret = ''
 
-access_token = '119448626-DnhFwmDmuG1YrkeLMI22hizPpd1BMPnOK55dF8A7'
-access_token_secret = 'P71ZdR6Ypsaou2LiVWWn5QllRGD9gYsV8OPalrjeRzjpY'
+#Add Access Token and Access Token Secret from https://developer.twitter.com
+access_token = ''
+access_token_secret = ''
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
